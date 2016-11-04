bdb7b8d0b21a4017f6dd2c7af959eee24c950ebc
Nick Shelley <nickmshelley@gmail.com>
2016-11-04 10:45:16 -0600

    Update to Swift 3.

---
+++ RichTextVC-iOS/RichTextVC-iOSUITests/RichTextVC_iOSUITests.swift
@@ -0,0 +1,13 @@
+//
+//  RichTextVC_iOSUITests.swift
+//  RichTextVC-iOSUITests
+//
+//  Created by Nick Shelley on 11/4/16.
+//  Copyright © 2016 Company. All rights reserved.
+//
+
+import XCTest
+
+class RichTextVC_iOSUITests: XCTestCase {
+    
+}
